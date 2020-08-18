b0a5261a5691c60988824acd3a63df5d68e64f66
dedsuh <65213088+dedsuh@users.noreply.github.com>
2020-08-19 03:33:24 +0700

    Add files via upload

---
+++ animasi.js
@@ -0,0 +1,29 @@
+
+		$(document).ready(function () {
+			$('#namawebdesktop').show(3000);
+			$('#img4').fadeIn(5000, function () {
+				$('#img4').hide();
+				$('#img2').fadeIn(5000, function () {
+					$('#img2').hide();
+					$('#img3').fadeIn(5000, function () {
+						$('#img3').hide();
+						$('#img6').fadeIn(5000, function () {
+							$('#img6').hide();
+							$('#img5').fadeIn(5000, function () {
+								$('#img5').hide();
+								$('#img7').fadeIn(5000, function () {
+									$('#img7').hide();
+									$('#img8').fadeIn(5000, function () {
+										$('#img8').hide();
+										$('#img9').fadeIn(5000, function () {
+											$('#img9').hide();
+											$('#img1').fadeIn(5000);
+										});
+									});
+								});
+							});
+						});
+					});
+				});
+			});
+		});
